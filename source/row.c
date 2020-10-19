0f337c3110c795aca866948cc851f85624553507
GabrielMtins <gabrielmartinsthe12381@gmail.com>
2020-10-19 14:25:38 -0300

    Added license

--- source/row.c
+++ source/row.c
@@ -1,6 +1,24 @@
 #include "row.h"
 #include <stdlib.h>
 
+/*
+    Copyright(C) 2020 Gabriel Martins
+    This file is part of GCTE.
+
+    GCTE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GCTE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GCTE.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 row* row_create(){
     row* new_row = malloc(sizeof(row));
     new_row->size = 1;
